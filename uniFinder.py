4ec56fd60a27c96085eb0d0400a80fdb02c74325
Dalton Hulett <dhulett3@huskers.unl.edu>
2022-04-08 14:42:20 -0500

    Separate columns and data

--- uniFinder.py
+++ uniFinder.py
@@ -5,20 +5,25 @@ from neo4j import GraphDatabase
 neoURL = "neo4j+s://2a882d3a.databases.neo4j.io:7687"
 neoUser = "neo4j"
 neoPassword = "cZWVr8MUErrFaJlXy88rKXMkwBAaWrdnkgV6B1-vfHg"
-fileName = "/content/drive/MyDrive/CSCE410_2022S/Final_Projects/Team 3/uni_data.csv"
+fileName = "uni_data.csv"
 
 class UniFinder:
 
   def __init__(self, uri, user, password):
     self.driver = GraphDatabase.driver(uri, auth=(user, password))
+    self.data = []
     self.columns = []
 
 
   def readData(self, fileName):
     with open(fileName, newline='') as csvfile:
-      spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-      for row in spamreader:
-        print(', '.join(row))
+      reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+      isColumns = True
+      for row in reader:
+        if (isColumns):
+          self.columns.append(row)
+          isColumns = False
+        self.data.append(row)
 
 
   def close(self):
